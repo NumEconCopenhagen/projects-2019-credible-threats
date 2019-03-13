140bc8f240f1a8580f02b816805fb0803f7d08ed
Sebastian Hørlück <hng309@alumni.ku.dk>
2019-03-13 14:24:28 +0100

    Data set

---
+++ dataproject/data_gen.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import os
+
+filename = 'projects-2019-credible-threats\dataproject\imdb.csv'
+
+test = pd.read_csv(filename, sep=';', encoding='latin-1', escapechar='\\')
+
+df = pd.DataFrame(test)
+
+for i in range(44,48):
+    df.drop(columns=[f'Unnamed: {i}'], inplace=True)
+
+df.drop(columns=['fn','tid','wordsInTitle','url'], inplace=True)
+I = df['imdbRating']
+print(df.loc[[8883]])
+
+
+
