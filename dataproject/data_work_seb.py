c8f9a374e717937217aca2e0270d6457c7e92796
Sebastian Hørlück <hng309@alumni.ku.dk>
2019-03-15 21:48:40 +0100

    Plot 4 figures

--- dataproject/data_work_seb.py
+++ dataproject/data_work_seb.py
@@ -1,22 +1,29 @@
 import matplotlib.pyplot as plt 
+import matplotlib as ax
 from data_gen_test import gen_df
+import numpy as np
 
 filename = 'imdb.csv'
 
 df = gen_df(filename)
+I = df['year']<1940
+df.drop(df[I].index,inplace=True)
+df['year'] = df['year'].astype(int)
 
-plt.figure(1)
+fig,ax = plt.subplots()
 
+# Plot year sum of different genres
 for j,i in enumerate(['Action','Western','War','Drama'],1):
     y_sum = df.groupby('year')[i].sum()
-    print(j)
     
-    plt.subplot(2,2,j)
+    ax = plt.subplot(2,2,j)
     y_sum.plot(kind='bar')
 
-    plt.xticks([])
+    n = 10  # Keeps every 7th label
+    [l.set_visible(False) for (i,l) in enumerate(ax.xaxis.get_ticklabels()) if i % n != 0]
+
     plt.xlabel('year')
     plt.ylabel(i)
     
-
 plt.show()
+
