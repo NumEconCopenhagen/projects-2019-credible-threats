0f72d37539ae3345d1dd8327f50dd87eba20596a
Sebastian Hørlück <hng309@alumni.ku.dk>
2019-03-15 11:10:59 +0100

    Import data as function

---
+++ dataproject/data_gen_test.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+#import os
+
+filename = 'imdb.csv'
+
+def gen_df(filename):
+
+    # Get .csv.file
+    data = pd.read_csv(filename, sep=';', encoding='latin-1', escapechar='\\')
+
+    # Read file into pandas dataframe
+    df = pd.DataFrame(data)
+
+    # Drop unwanted columns
+    for i in range(44,48):
+        df.drop(columns=[f'Unnamed: {i}'], inplace=True)
+
+    df.drop(columns=['fn','tid','wordsInTitle','url'], inplace=True)
+
+    # Keep only observations of movie-type
+    I = df['type'] == 'video.movie'
+    df = df.loc[I]
+    df.drop(columns=['type'], inplace=True)
+
+    # Drop observations with missing data
+    df.dropna(inplace=True)
+
+    # Replace 0's in imdb-ratings
+    df['imdbRating'] = df['imdbRating'].astype(str)
+    df['imdbRating'].replace(regex=True, inplace=True,to_replace='0',value='')
+    df['imdbRating'] = df['imdbRating'].astype(float)
+
+    # Transform duration from seconds to hours
+    df['duration'] = df['duration']/60**2
+
+    return df
---
+++ dataproject/data_work_seb.py
@@ -0,0 +1,7 @@
+from data_gen_test import gen_df
+
+filename = 'imdb.csv'
+
+df = gen_df(filename)
+
+print(df.head())
