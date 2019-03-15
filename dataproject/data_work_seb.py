adc1cdc7d0bbe54ccb2d72e87c7fb3fc867ba8d0
Sebastian Hørlück <hng309@alumni.ku.dk>
2019-03-15 11:54:26 +0100

    Plot number of genres

--- dataproject/data_work_seb.py
+++ dataproject/data_work_seb.py
@@ -1,7 +1,22 @@
+import matplotlib.pyplot as plt 
 from data_gen_test import gen_df
 
 filename = 'imdb.csv'
 
 df = gen_df(filename)
 
-print(df.head())
+plt.figure(1)
+
+for j,i in enumerate(['Action','Western','War','Drama'],1):
+    y_sum = df.groupby('year')[i].sum()
+    print(j)
+
+    plt.subplot(4,1,j)
+    y_sum.plot(kind='bar')
+
+    #plt.hist(y_sum[:])
+    plt.xticks([])
+    plt.xlabel('year')
+    plt.ylabel(i)
+
+plt.show()
