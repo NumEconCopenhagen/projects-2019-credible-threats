358d09d63ce7772bbaf2cded59b8219c086387d6
Sebastian Hørlück <hng309@alumni.ku.dk>
2019-03-20 15:49:32 +0100

    Data seb

--- dataproject/data_work_seb.py
+++ dataproject/data_work_seb.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt 
 from data_gen import gen_df
 import numpy as np
+from bs4 import BeautifulSoup as bs
 import requests
+import pandas as pd
 
 
 filename = 'imdb.csv'
@@ -54,9 +56,30 @@ def top_bottom_movies(df):
 
     top = df.sort_values('imdbRating', ascending=False)[I]
     bottom = df.sort_values('imdbRating', ascending=True)[I]
-    print(top.head(3), str('\n'), bottom.head(3))
 
     # Get English titles 
+    top_id = top.tid.tolist()[0:10]
+    bottom_id = bottom.tid.tolist()[0:10]
+    id_list = top_id + bottom_id
+
+    eng_names = []
+    
+    for i,id in enumerate(id_list):
+        print(id)
+        test = requests.get('https://www.imdb.com/title/' + id)
+        soup = bs(test.text,'html.parser')
+        if soup.find('div',class_='originalTitle') != None:
+            eng_names.append(soup.find('div',class_='originalTitle').text)
+        else:
+            eng_names.append(soup.find('h1').text)
+        print(i)
+
+    df_merge = pd.DataFrame(i for i in eng_names)
+    
+    df_merge.rename(columns={'0': 'tid'})
+
+    #test = df.merge(df_merge,how='right',on='tid')
+    #print(test.head())
 
 
 top_bottom_movies(df)
