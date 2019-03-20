984030ac5511524af3d256ccbb2bacbfe2454cf4
Sebastian Hørlück <hng309@alumni.ku.dk>
2019-03-20 13:16:06 +0100

    Data seb

--- dataproject/data_work_seb.py
+++ dataproject/data_work_seb.py
@@ -1,40 +1,48 @@
 import matplotlib.pyplot as plt 
-import matplotlib as ax
-from data_gen_test import gen_df
+from data_gen import gen_df
 import numpy as np
 
 
 filename = 'imdb.csv'
 
 df = gen_df(filename)
-I = df['year']<1920
-df.drop(df[I].index,inplace=True)
 df['year'] = df['year'].astype(int)
 
-fig,ax = plt.subplots()
+def plot_1(df):
 
-# Plot year sum of different genres
-for j,i in enumerate(['Action','Western','War','Animation'],1):
-    y_sum = df.groupby('year')[i].sum()
-    
-    ax = plt.subplot(2,2,j)
-    y_sum.plot(kind='bar', color='black')
+    fig,ax = plt.subplots()
 
-    n = 10  # Keeps every 10th label
-    [l.set_visible(False) for (i,l) in enumerate(ax.xaxis.get_ticklabels()) if i % n != 0]
+    # Plot year sum of different genres
+    for j,i in enumerate(['Action','Western','War','Animation'],1):
+        y_sum = df.groupby('year')[i].sum()
+        
+        ax = plt.subplot(2,2,j)
+        y_sum.plot(kind='bar', color='black')
 
-    plt.xlabel('year')
-    plt.ylabel(i)
-    
-#plt.show()
+        # Keeps every 10th label
+        n = 10  
+        [l.set_visible(False) for (i,l) in enumerate(ax.xaxis.get_ticklabels()) if i % n != 0]
 
-# Search for string in dataframe
-print(list(df))
-df['NomWinRatio'] = df['nrOfWins']/df['nrOfNominations']
-I = df['NomWinRatio'] <= 0.2
-#print(df.loc[I])
-df.plot.scatter(x='NomWinRatio', y='nrOfNewsArticles')
-plt.show()
+        plt.xlabel('year')
+        plt.ylabel(i)
+        
+    plt.show()
 
-#print(df[df['title'].str.contains('')])
-#print(df['nrofNewsArticles'])
+def plot_2(df):
+    df['NomWinRatio'] = df['nrOfWins']/df['nrOfNominations']
+
+    genre_list = ['Action','Animation']
+
+    for i in genre_list:
+        I = df[i] == 1
+
+        x = df.loc[I, 'ratingCount']
+        y = df.loc[I, 'nrOfWins']
+
+        plt.get_xaxis().set_major_formatter(plt.FuncFormatter(lambda x: '{:,}').format(int(x)))
+
+    plt.legend(loc='upper')
+    plt.show()
+
+plot_2(df)
+#print(df.ratingCount.head())
