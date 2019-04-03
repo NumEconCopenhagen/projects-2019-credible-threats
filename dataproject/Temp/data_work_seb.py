a0602381841aaf52dd844f9538e2b214ae0103e6
Jakob Langager Christensen <Admin@Jakobs-MacBook-Air-2.local>
2019-04-03 14:29:54 +0200

    Delete

---
+++ dataproject/Temp/data_work_seb.py
@@ -0,0 +1,86 @@
+import matplotlib.pyplot as plt 
+from data_gen import gen_df
+import numpy as np
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
+
+
+filename = 'imdb.csv'
+
+df = gen_df(filename)
+df['year'] = df['year'].astype(int)
+
+def plot_1(df):
+    
+    df['count'] = 1
+    df['movies_year'] = df.groupby('year')['count'].transform(lambda x: x.sum())  
+
+    # Plot year sum of different genres
+    for j,i in enumerate(['Crime','Western','War','Biography'],1):
+        df[f'{i}_year'] = df.groupby('year')[i].transform(lambda x: x.sum())
+        df[f'{i}_share'] = df[f'{i}_year']/df['movies_year']
+
+        y_share = df.groupby('year')[f'{i}_share'].first()
+        
+        ax = plt.subplot(2,2,j)
+        y_share.plot(kind='line', color='black', sharex='col', sharey='row')
+
+        # Keeps every 10th label
+        #n = 10  
+        #[l.set_visible(False) for (i,l) in enumerate(ax.xaxis.get_ticklabels()) if i % n != 0]
+
+        plt.xlabel('year')
+        plt.ylabel('share of movies')
+        plt.title(i)
+        
+    plt.show()
+
+def plot_2(df):
+    df['NomWinRatio'] = df['nrOfWins']/df['nrOfNominations']
+
+    genre_list = ['Action','Animation']
+
+    for i in genre_list:
+        I = df[i] == 1
+
+        x = df.loc[I, 'ratingCount']
+        y = df.loc[I, 'nrOfWins']
+
+    plt.legend(loc='upper')
+    plt.show()
+
+def top_bottom_movies(df):
+    # Condition on minimum number of ratings
+    I = df['ratingCount'] >= 2000
+
+    top = df.sort_values('imdbRating', ascending=False)[I]
+    bottom = df.sort_values('imdbRating', ascending=True)[I]
+
+    # Get English titles 
+    top_id = top.tid.tolist()[0:10]
+    bottom_id = bottom.tid.tolist()[0:10]
+    id_list = top_id + bottom_id
+
+    eng_names = []
+    
+    for i,id in enumerate(id_list):
+        print(id)
+        test = requests.get('https://www.imdb.com/title/' + id)
+        soup = bs(test.text,'html.parser')
+        if soup.find('div',class_='originalTitle') != None:
+            eng_names.append(soup.find('div',class_='originalTitle').text)
+        else:
+            eng_names.append(soup.find('h1').text)
+        print(i)
+
+    df_merge = pd.DataFrame(i for i in eng_names)
+    
+    df_merge.rename(columns={'0': 'tid'})
+
+    #test = df.merge(df_merge,how='right',on='tid')
+    #print(test.head())
+
+
+top_bottom_movies(df)
+
---
+++ dataproject/Temp/ida.py
@@ -0,0 +1,136 @@
+# Stjæler lidt kode fra Seb
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import ipywidgets as widgets
+
+filename = 'imdb.csv'
+
+test = pd.read_csv(filename, sep=';', encoding='latin-1', escapechar='\\')
+
+df = pd.DataFrame(test)
+print(df.shape)
+
+for i in range(44,48):
+    df.drop(columns=[f'Unnamed: {i}'], inplace=True)
+
+df.drop(columns=['fn','tid','wordsInTitle','url'], inplace=True)
+I = df['imdbRating']
+print(df.shape)
+
+
+I = df['type'] == 'video.movie'
+df = df.loc[I]
+df.drop(columns=['type'], inplace=True)
+print(df.shape)
+
+df.dropna(inplace=True)
+print(df.shape)
+
+df['imdbRating'] = df['imdbRating'].astype(str)
+df['imdbRating'].replace(regex=True, inplace=True,to_replace='0',value='')
+print(type(df['imdbRating'][0]))
+df['imdbRating'] = df['imdbRating'].astype(float)
+
+df['duration'] = df['duration']/60**2
+
+test = np.array([1, 2, 3])
+
+#df.to_excel('imdb_test.xlsx', index=False)
+print(df.head(8))
+
+
+#####################################################################################
+# Data Work
+
+# Data types 
+print(df.dtypes)
+
+# Summary Statistics 
+print(df[["year", "imdbRating", "duration", "nrOfWins", "nrOfNominations"]].describe())
+
+
+# Generating genre list
+genre = ['Action', 'Adult', 'Adventure', 'Animation', 'Biography',
+         'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy',
+         'FilmNoir', 'GameShow', 'History', 'Horror', 'Music', 'Musical',
+         'Mystery', 'News', 'RealityTV', 'Romance', 'SciFi', 'Short', 'Sport',
+         'TalkShow', 'Thriller', 'War', 'Western']
+
+
+# Counting movies in each genre
+for i in genre: 
+    sum = df[i].sum(axis=0)
+    print(f'Sum of {i}: {sum:.0f}')
+
+
+#####################################################################################
+# Figures
+#####################################################################################
+
+# Histogram
+import seaborn as sns
+sns.set()
+
+# plt.hist(df["imdbRating"], bins=10)
+
+
+# Histogram by genre
+df_hist = df.loc[df["Comedy"] == 1]
+plt.hist(df_hist["imdbRating"], bins=10)
+
+
+# Interactive Histogram
+def histogram(variable, genre, bins_num):
+        df_hist = df.loc[df[genre] == 1]
+        plt.hist(df_hist[variable], bins=bins_num)
+    
+        # Labels
+        plt.title("Histogram")
+        plt.ylabel("Number")
+        plt.xticks(range(1,11))
+    
+# histogram("imdbRating", "Comedy")   # Remember quote marks around variable names
+# Consider writing an error code indicating to use quote marks
+
+
+def hist_interactive(variable): 
+    widgets.interact(histogram, 
+    variable = widgets.fixed(variable), 
+    
+    # Genre Dropdown
+    genre = widgets.Dropdown(
+    description="Genre", 
+    options=genre, 
+    value="Comedy"),
+    
+    # Bins slider 
+    bins_num=widgets.IntSlider(
+    value=10,
+    description="Bins", 
+    min=1,
+    max=50, 
+    step=1,
+    disabled=False,
+    continuous_update=False)
+    ); 
+
+plt.show()
+hist_interactive("imdbRating")
+
+#####################################################################################
+
+# Scatter X against years
+def scatter(variable):
+    plt.figure(figsize=(11,8))
+    plt.scatter(df['year'], variable)
+    
+    plt.title("Scatterplot")
+    plt.xlabel("Year")
+
+scatter(df["imdbRating"])    
+
+
+print(df[df["nrOfWins"] == 137])
+print(df[df["nrOfNominations"] == 137])
+
